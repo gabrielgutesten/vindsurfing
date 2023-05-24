43ce8b5f673f6bbfca3e08cf78484ee5d797f723
gabrielgutesten <gabriel.gutesten@student.tabyenskilda.se>
2023-05-24 23:04:16 +0200

    24 Maj

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 window.onload = function() {
   const menu_btn = document.querySelector('.hamburger');
   const site_content = document.querySelector('.sidenav');
-
+  const heart_btn = document.querySelector('.material-symbols-outlined');
 
   menu_btn.addEventListener('click', function() {
     menu_btn.classList.toggle('is-active');
     site_content.classList.toggle('is-active');
-
-    
   });
-}
+
+  heart_btn.addEventListener('click', function() {
+    heart_btn.classList.toggle('is-active');
+  });
+};
