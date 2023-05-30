ab4a4a83a89ba8cf910291b5ce9e5d5e0f89c34a
gabrielgutesten <gabriel.gutesten@student.tabyenskilda.se>
2023-05-30 19:39:20 +0200

    30 maj

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 window.onload = function() {
+  // Hämtar DOM-elementen
   const menu_btn = document.querySelector('.hamburger');
   const site_content = document.querySelector('.sidenav');
   const heart_btn = document.querySelector('.material-symbols-outlined');
 
-
+  // Lägger till en händelselyssnare för klick på menyknappen
   menu_btn.addEventListener('click', function() {
+    // Växlar klassen 'is-active' på menyknappen
     menu_btn.classList.toggle('is-active');
+    // Växlar klassen 'is-active' på sidans innehåll
     site_content.classList.toggle('is-active');
   });
-// ändrar html klassen från "sidenav" till "sidenav.is-active"
-
+  // Genom att klicka på hjärtsymbolen ändras klassen 'is-active'
   heart_btn.addEventListener('click', function() {
     heart_btn.classList.toggle('is-active');
   });
-// Om man trycker på hjärtsymbolen ändras html klassen så att "is-active" står efter
 };
